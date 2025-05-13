4f160e41586d474a8cf405b421961be5d852441a
Nobuhiro Oto <nobuhirooto@gmail.com>
2025-05-13 14:20:24 +0900

    nvim updates

--- ubuntu/nvim/lua/notoh/core/options.lua
+++ ubuntu/nvim/lua/notoh/core/options.lua
@@ -3,13 +3,6 @@ vim.cmd("let g:netrw_banner = 0")
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4 
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
--- ubuntu/nvim/lua/notoh/lazy.lua
+++ ubuntu/nvim/lua/notoh/lazy.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
     {
         { import = "notoh.plugins" },
-        { import = "notoh.plugins.lsp" },
     },
     {
         checker = {
